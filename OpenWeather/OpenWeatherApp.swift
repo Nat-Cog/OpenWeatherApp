6577ca5fdde31e1f34cb35636b5ea18d47fdc5cd
Nat-Cog <115486821+Nat-Cog@users.noreply.github.com>
2023-05-16 09:08:30 -0500

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by m_959058 on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
